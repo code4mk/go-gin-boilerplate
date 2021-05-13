71477325e63c2b8789a7f2387c6d599caab18f2a
mostafa <radionishi@gmail.com>
2021-05-13 23:36:20 +0600

    gorm

--- go.mod
+++ go.mod
@@ -7,4 +7,6 @@ require (
 	github.com/gin-gonic/gin v1.7.1
 	github.com/jasonlvhit/gocron v0.0.1
 	github.com/joho/godotenv v1.3.0
+	gorm.io/driver/mysql v1.0.6
+	gorm.io/gorm v1.21.9
 )
--- main.go
+++ main.go
@@ -2,21 +2,38 @@ package main
 
 import (
 	"fmt"
+	"goginapi/model"
 	"goginapi/routes"
-	"goginapi/schedule"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	schedule.Task()
+
+	fmt.Println("mm")
+
+	// schedule.Task()
 	err := godotenv.Load()
 
 	if err != nil {
 		fmt.Println("failed to find .env file")
 	}
+
+	dsn := "root:mysql@tcp(127.0.0.1:3306)/gogin?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("failed to find .env file")
+	}
+
+	user := model.User{Name: "Jinzhu", Age: 18}
+
+	db.AutoMigrate(&user, model.Book{})
+
+	db.Create(&user)
 
 	// declare router
 	router := gin.Default()
